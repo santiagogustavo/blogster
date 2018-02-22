062d0f4b8e644a9b84bd32330dc49a4de4adbf97
santiagogustavo <gs4n.96@gmail.com>
2018-02-22 18:23:23 -0300

    (+) Style modifications and fixes, adds a header to pages and stylizes post show

--- blogster/app/controllers/posts_controller.rb
+++ blogster/app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
     if params[:category].blank?
       @posts = Post.all.order('created_at DESC')
     else
+      @category_name = params[:category]
       @category_id = Category.find_by(name: params[:category])
       @posts = Post.where(category_id: @category_id).order('created_at DESC')
     end
