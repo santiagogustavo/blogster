6749e1fc27842e430fbc9a99f1dc318695925d8b
santiagogustavo <gs4n.96@gmail.com>
2018-02-23 14:39:55 -0300

    (+) Categories controller added, new features based on the tags
    Category controller added, for model management. Button on the navbar
    links to the Category creation, instead of Post creation;
    
    Posts are now created with the blue button placed after the page title
    OR in the blank page screen, replacing the previous message;
    
    Now, post creation when browsing a certain Category will prompt the form
    with that same Category selected by default OR a placeholder message if
    the post is created at Home.

---
+++ blogster/app/controllers/categories_controller.rb
@@ -0,0 +1,48 @@
+# CATEGORY CONTROLLER
+# MODEL: Category (:name)
+class CategoriesController < ApplicationController
+  before_action :find_category, only: %i[edit update destroy]
+
+  # Generates an empty instance for Category model
+  def new
+    @category = Category.new
+  end
+
+  # Generates an instance for Category model with the parameters filled
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to posts_path(category: @category.name)
+    else
+      render 'new'
+    end
+  end
+
+  # Passthrough for getting the correct Category with before_action
+  def edit; end
+
+  def update
+    if @category.update(post_params)
+      redirect_to posts_path(category: @category.name)
+    else
+      render 'edit'
+    end
+  end
+
+  # Obtains the correct Category with before_action and destroys it
+  def destroy
+    @category.destroy
+    redirect_to root_path, notice: 'Category destroyed successfully!'
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
+end
