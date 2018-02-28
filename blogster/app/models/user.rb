d5c98a4e9a75701ff2d2614c3ff6ebb289fa30cb
santiagogustavo <gs4n.96@gmail.com>
2018-02-28 11:12:53 -0300

    (+) Adds user session control, with login and logout properly working
    - Username attribute renamed to Name, avoiding redundancies in attribute access
    
    - User informations added to navbar, including a logout button for session control

--- blogster/app/models/user.rb
+++ blogster/app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates_presence_of :username
-  validates_uniqueness_of :username
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
